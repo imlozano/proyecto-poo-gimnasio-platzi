9935202d3c14220b4f8243b1f361a5ca00733596
Miguel Lozano <miguelozanodev@gmail.com>
2026-09-14 00:53:22 -0500

    Centralizar reglas de validacion para no duplicarse, Cambio de nombre de Routines a Routine en singular

--- src/app.js
+++ src/app.js
@@ -1,18 +1,49 @@
 let routineIdCounter = 0;
 
-function Routines(name, series, repetitionsPerSet, rest) {
-
-    if (name.trim().length < 3) {
-        throw new Error('El nombre de la rutina debe tener al menos 3 caracteres.')
-    } else if (series < 2 || series > 5) {
-        throw new Error('El nuevo valor es superior a 5 series o es mejor a 5 series. Recuerda que no te puedes exceder y no puedes no hacer nada.')
-    } else if (repetitionsPerSet < 5 || repetitionsPerSet > 30) {
-        throw new Error('El valor de las repeticiones por serie es muy alto o muy bajo. Recuerda que el máximo son 30. Empieza de a poco 😉.')
-    } else if (rest < 30 || rest > 60) {
-        throw new Error('El descanso máximo es de un minuto. Mas te puede dar pereza 🦥. Y menos tiempo te puede dar fatiga. El rango es de 30 a 60 segundos.')
+const ROUTINE_VALIDATION = {
+    name: {
+        minLength: 3,
+        errorMessage: 'El nombre de la rutina debe tener al menos 3 caracteres.'
+    },
+    series: {
+        min: 2,
+        max: 5,
+        errorMessage: 'El nuevo valor es superior a 5 series o es mejor a 5 series. Recuerda que no te puedes exceder y no puedes no hacer nada.'
+    },
+    repetitionsPerSet: {
+        min: 5,
+        max: 30,
+        errorMessage: 'El valor de las repeticiones por serie es muy alto o muy bajo. Recuerda que el máximo son 30. Empieza de a poco 😉.'
+    },
+    rest: {
+        min: 30,
+        max: 60,
+        errorMessage: 'El descanso máximo es de un minuto. Mas te puede dar pereza 🦥. Y menos tiempo te puede dar fatiga. El rango es de 30 a 60 segundos.'
     }
+};
 
+function validateName(name) {
+    if (name.trim().length < ROUTINE_VALIDATION.name.minLength) {
+        throw new Error(ROUTINE_VALIDATION.name.errorMessage);
+    }
+}
 
+function validateRange(value, rule) {
+    if (value < rule.min || value > rule.max) {
+        throw new Error(rule.errorMessage);
+    }
+}
+
+function validateRoutine(name, series, repetitionsPerSet, rest) {
+    validateName(name);
+    validateRange(series, ROUTINE_VALIDATION.series);
+    validateRange(repetitionsPerSet, ROUTINE_VALIDATION.repetitionsPerSet);
+    validateRange(rest, ROUTINE_VALIDATION.rest);
+}
+
+function Routine(name, series, repetitionsPerSet, rest) {
+
+    validateRoutine(name, series, repetitionsPerSet, rest);
 
     // Propiedades
     this.id = ++routineIdCounter;
@@ -28,45 +59,37 @@ function Routines(name, series, repetitionsPerSet, rest) {
 }
 
 // Comportamientos
-Routines.prototype.rename = function rename(newName) {
-    if (newName.trim().length < 3) {
-        throw new Error('El nombre de la rutina debe tener al menos 3 caracteres.')
-    }
+Routine.prototype.rename = function rename(newName) {
+    validateName(newName);
     this.name = newName.trim();
 
 }
 
-Routines.prototype.changeCountSeries = function changeCountSeries(newValue) {
-    if (newValue < 2 || newValue > 5) {
-        throw new Error('El nuevo valor es superior a 5 series o es mejor a 5 series. Recuerda que no te puedes exceder y no puedes no hacer nada.')
-    }
+Routine.prototype.changeCountSeries = function changeCountSeries(newValue) {
+    validateRange(newValue, ROUTINE_VALIDATION.series);
 
     this.series = newValue;
     this.durationRoutine = this.calculateDuration()
 
 }
 
-Routines.prototype.changeRepetitionsPerSet = function changeRepetitionsPerSet(newValue) {
-    if (newValue < 5 || newValue > 30) {
-        throw new Error('El valor de las repeticiones por serie es muy alto o muy bajo. Recuerda que el máximo son 30. Empieza de a poco 😉.')
-    }
+Routine.prototype.changeRepetitionsPerSet = function changeRepetitionsPerSet(newValue) {
+    validateRange(newValue, ROUTINE_VALIDATION.repetitionsPerSet);
 
     this.repetitionsPerSet = newValue;
     this.durationRoutine = this.calculateDuration()
 
 }
 
-Routines.prototype.changeRest = function changeRest(newValue) {
-    if (newValue < 30 || newValue > 60) {
-        throw new Error('El descanso máximo es de un minuto. Mas te puede dar pereza 🦥. Y menos tiempo te puede dar fatiga. El rango es de 30 a 60 segundos.')
-    }
+Routine.prototype.changeRest = function changeRest(newValue) {
+    validateRange(newValue, ROUTINE_VALIDATION.rest);
 
     this.rest = newValue;
     this.durationRoutine = this.calculateDuration()
 
 }
 
-Routines.prototype.calculateDuration = function calculateDuration() {
+Routine.prototype.calculateDuration = function calculateDuration() {
     const repetitions = this.repetitionsPerSet
     const durationPerSet = this.durationPerSet
     const calculateExecution = repetitions * durationPerSet;
--- src/examples.js
+++ src/examples.js
@@ -1,17 +1,50 @@
+const ROUTINE_VALIDATION = {
+    name: { minLength: 3 },
+    series: { min: 2, max: 5 },
+    repetitionsPerSet: { min: 5, max: 30 },
+    rest: { min: 30, max: 60 }
+};
+
+const VALIDATION_MESSAGES = {
+    object: {
+        name: 'El nombre de la rutina debe tener al menos 3 caracteres',
+        series: 'El nuevo valor es superior a 5 series o es menor a 5 series. Recuerda que no te puedes exceder y no puedes no hacer nada.',
+        repetitionsPerSet: 'El valor de las repeticiones por serie es muy alto o muy bajo. Recuerda que el máximo son 30. Empieza de a poco 😉',
+        rest: 'El descanso máximo es de un minuto. Mas te puede dar pereza 🦥. Y menos tiempo te puede dar fatiga'
+    },
+    class: {
+        name: 'El nombre de la rutina debe tener al menos 3 caracteres.',
+        series: 'El nuevo valor es superior a 5 series o es menor a 5 series. Recuerda que no te puedes exceder y no puedes no hacer nada.',
+        repetitionsPerSet: 'El valor de las repeticiones por serie es muy alto o muy bajo. Recuerda que el máximo son 30. Empieza de a poco 😉.',
+        rest: 'El descanso máximo es de un minuto. Mas te puede dar pereza 🦥. Y menos tiempo te puede dar fatiga. El rango es de 30 a 60 segundos.'
+    }
+};
+
+function validateName(name, errorMessage) {
+    if (name.trim().length < ROUTINE_VALIDATION.name.minLength) {
+        throw new Error(errorMessage);
+    }
+}
+
+function validateRange(value, rule, errorMessage) {
+    if (value < rule.min || value > rule.max) {
+        throw new Error(errorMessage);
+    }
+}
+
+function validateRoutine(name, series, repetitionsPerSet, rest, messages) {
+    validateName(name, messages.name);
+    validateRange(series, ROUTINE_VALIDATION.series, messages.series);
+    validateRange(repetitionsPerSet, ROUTINE_VALIDATION.repetitionsPerSet, messages.repetitionsPerSet);
+    validateRange(rest, ROUTINE_VALIDATION.rest, messages.rest);
+}
+
 // Objeto literal con estado + comportamiento
 let routineIdCounterTwo = 0;
 
-function createRoutinesObject(name, series, repetitionsPerSet, rest) {
+function createRoutineObject(name, series, repetitionsPerSet, rest) {
 
-    if (name.trim().length < 3) {
-        throw new Error('El nombre de la rutina debe tener al menos 3 caracteres')
-    } else if (series < 2 || series > 5) {
-        throw new Error('El nuevo valor es superior a 5 series o es menor a 5 series. Recuerda que no te puedes exceder y no puedes no hacer nada.')
-    } else if (repetitionsPerSet < 5 || repetitionsPerSet > 30) {
-        throw new Error('El valor de las repeticiones por serie es muy alto o muy bajo. Recuerda que el máximo son 30. Empieza de a poco 😉')
-    } else if (rest < 30 || rest > 60) {
-        throw new Error('El descanso máximo es de un minuto. Mas te puede dar pereza 🦥. Y menos tiempo te puede dar fatiga')
-    }
+    validateRoutine(name, series, repetitionsPerSet, rest, VALIDATION_MESSAGES.object);
 
     const routine = {
 
@@ -27,17 +60,13 @@ function createRoutinesObject(name, series, repetitionsPerSet, rest) {
 
         // Comportamientos
         rename: function (newName) {
-            if (newName.trim().length < 3) {
-                throw new Error('El nombre de la rutina debe tener al menos 3 caracteres')
-            }
+            validateName(newName, VALIDATION_MESSAGES.object.name);
             this.name = newName.trim();
 
         },
 
         changeCountSeries: function (newValue) {
-            if (newValue < 2 || newValue > 5) {
-                throw new Error('El nuevo valor es superior a 5 series o es menor a 5 series. Recuerda que no te puedes exceder y no puedes no hacer nada.')
-            }
+            validateRange(newValue, ROUTINE_VALIDATION.series, VALIDATION_MESSAGES.object.series);
 
             this.series = newValue;
             this.durationRoutine = this.calculateDuration()
@@ -45,9 +74,7 @@ function createRoutinesObject(name, series, repetitionsPerSet, rest) {
         },
 
         changeRepetitionsPerSet: function (newValue) {
-            if (newValue < 5 || newValue > 30) {
-                throw new Error('El valor de las repeticiones por serie es muy alto o muy bajo. Recuerda que el máximo son 30. Empieza de a poco 😉')
-            }
+            validateRange(newValue, ROUTINE_VALIDATION.repetitionsPerSet, VALIDATION_MESSAGES.object.repetitionsPerSet);
 
             this.repetitionsPerSet = newValue;
             this.durationRoutine = this.calculateDuration()
@@ -55,9 +82,7 @@ function createRoutinesObject(name, series, repetitionsPerSet, rest) {
         },
 
         changeRest: function (newValue) {
-            if (newValue < 30 || newValue > 60) {
-                throw new Error('El descanso máximo es de un minuto. Mas te puede dar pereza 🦥. Y menos tiempo te puede dar fatiga')
-            }
+            validateRange(newValue, ROUTINE_VALIDATION.rest, VALIDATION_MESSAGES.object.rest);
 
             this.rest = newValue;
             this.durationRoutine = this.calculateDuration()
@@ -85,30 +110,22 @@ function createRoutinesObject(name, series, repetitionsPerSet, rest) {
 
 }
 
-const objeto1 = createRoutinesObject('Sentadilla', 2, 10, 30)
+const objeto1 = createRoutineObject('Sentadilla', 2, 10, 30)
 console.log(objeto1);
 objeto1.changeRepetitionsPerSet(5);
 console.log(objeto1);
 
 // rutina invalida
-const objeto2 = createRoutinesObject('Burpees', 20, 5, 45);
+const objeto2 = createRoutineObject('Burpees', 20, 5, 45);
 
 
 //const routines = [];
 
 let routineIdCounter = 0;
 
-function Routines(name, series, repetitionsPerSet, rest) {
+function Routine(name, series, repetitionsPerSet, rest) {
 
-    if (name.trim().length < 3) {
-        throw new Error('El nombre de la rutina debe tener al menos 3 caracteres.')
-    } else if (series < 2 || series > 5) {
-        throw new Error('El nuevo valor es superior a 5 series o es menor a 5 series. Recuerda que no te puedes exceder y no puedes no hacer nada.')
-    } else if (repetitionsPerSet < 5 || repetitionsPerSet > 30) {
-        throw new Error('El valor de las repeticiones por serie es muy alto o muy bajo. Recuerda que el máximo son 30. Empieza de a poco 😉.')
-    } else if (rest < 30 || rest > 60) {
-        throw new Error('El descanso máximo es de un minuto. Mas te puede dar pereza 🦥. Y menos tiempo te puede dar fatiga. El rango es de 30 a 60 segundos.')
-    }
+    validateRoutine(name, series, repetitionsPerSet, rest, VALIDATION_MESSAGES.class);
 
 
 
@@ -126,45 +143,37 @@ function Routines(name, series, repetitionsPerSet, rest) {
 }
 
 // Comportamientos
-Routines.prototype.rename = function rename(newName) {
-    if (newName.trim().length < 3) {
-        throw new Error('El nombre de la rutina debe tener al menos 3 caracteres.')
-    }
+Routine.prototype.rename = function rename(newName) {
+    validateName(newName, VALIDATION_MESSAGES.class.name);
     this.name = newName.trim();
 
 }
 
-Routines.prototype.changeCountSeries = function changeCountSeries(newValue) {
-    if (newValue < 2 || newValue > 5) {
-        throw new Error('El nuevo valor es superior a 5 series o es menor a 5 series. Recuerda que no te puedes exceder y no puedes no hacer nada.')
-    }
+Routine.prototype.changeCountSeries = function changeCountSeries(newValue) {
+    validateRange(newValue, ROUTINE_VALIDATION.series, VALIDATION_MESSAGES.class.series);
 
     this.series = newValue;
     this.durationRoutine = this.calculateDuration()
 
 }
 
-Routines.prototype.changeRepetitionsPerSet = function changeRepetitionsPerSet(newValue) {
-    if (newValue < 5 || newValue > 30) {
-        throw new Error('El valor de las repeticiones por serie es muy alto o muy bajo. Recuerda que el máximo son 30. Empieza de a poco 😉.')
-    }
+Routine.prototype.changeRepetitionsPerSet = function changeRepetitionsPerSet(newValue) {
+    validateRange(newValue, ROUTINE_VALIDATION.repetitionsPerSet, VALIDATION_MESSAGES.class.repetitionsPerSet);
 
     this.repetitionsPerSet = newValue;
     this.durationRoutine = this.calculateDuration()
 
 }
 
-Routines.prototype.changeRest = function changeRest(newValue) {
-    if (newValue < 30 || newValue > 60) {
-        throw new Error('El descanso máximo es de un minuto. Mas te puede dar pereza 🦥. Y menos tiempo te puede dar fatiga. El rango es de 30 a 60 segundos.')
-    }
+Routine.prototype.changeRest = function changeRest(newValue) {
+    validateRange(newValue, ROUTINE_VALIDATION.rest, VALIDATION_MESSAGES.class.rest);
 
     this.rest = newValue;
     this.durationRoutine = this.calculateDuration()
 
 }
 
-Routines.prototype.calculateDuration = function calculateDuration() {
+Routine.prototype.calculateDuration = function calculateDuration() {
     const repetitions = this.repetitionsPerSet
     const durationPerSet = this.durationPerSet
     const calculateExecution = repetitions * durationPerSet;
@@ -178,15 +187,15 @@ Routines.prototype.calculateDuration = function calculateDuration() {
 }
 
 
-// const objeto1 = createRoutinesObject('Sentadilla', 2, 10, 15)
+// const objeto1 = createRoutineObject('Sentadilla', 2, 10, 15)
 // console.log(objeto1);
-const rutina1 = new Routines('Sentadilla', 2, 10, 35)
+const rutina1 = new Routine('Sentadilla', 2, 10, 35)
 console.log(rutina1);
 rutina1.changeCountSeries(4);
 console.log(rutina1.calculateDuration())
 console.log(rutina1);
 
-const rutina2 = new Routines('Salto en estrella', 2, 30, 45)
+const rutina2 = new Routine('Salto en estrella', 2, 30, 45)
 console.log(rutina2);
 
 console.log(rutina1.calculateDuration === rutina2.calculateDuration)
