2940836d18c6745c37e8147043b9191e54e01f93
Miguel Lozano <miguelozanodev@gmail.com>
2026-09-23 12:52:31 -0500

    refactor: implementar encapsulación y validación de rutinas. Etapa 2

--- src/app.js
+++ src/app.js
@@ -1,5 +1,3 @@
-let routineIdCounter = 0;
-
 const routines = [];
 
 const ROUTINE_VALIDATION = {
@@ -36,67 +34,113 @@ function validateInteger(value, rule) {
     }
 }
 
-function validateRoutine(name, series, repetitionsPerSet, rest) {
-    validateName(name);
+
+class Routine {
+    #id;
+    #name;
+    #series;
+    #repetitionsPerSet;
+    #rest;
+    #durationPerSet;
+    #durationRoutine;
+    #createdAt;
+
+    static #idCounter = 0;
 
 
-    validateInteger(series, ROUTINE_VALIDATION.series);
-    validateInteger(repetitionsPerSet, ROUTINE_VALIDATION.repetitionsPerSet);
-    validateInteger(rest, ROUTINE_VALIDATION.rest);
+    constructor(name, series, repetitionsPerSet, rest) {
+
+        this.#id = Routine.generateId();
+        this.name = name;
+        this.#durationPerSet = 5;
+        this.series = series;
+        this.repetitionsPerSet = repetitionsPerSet;
+        this.rest = rest;
+        this.#durationRoutine = this.calculateDuration();
+        this.#createdAt = new Date().toISOString();
+    }
+
+    get id() {
+        return this.#id;
+    }
+
+    get name() {
+        return this.#name;
+    }
+
+    set name(value) {
+        validateName(value);
+        this.#name = value.trim();
+    }
+
+    get series() {
+        return this.#series;
+    }
+
+    set series(value) {
+        validateInteger(value, ROUTINE_VALIDATION.series);
+        this.#series = value;
+    }
+
+    get repetitionsPerSet() {
+        return this.#repetitionsPerSet;
+    }
+
+    set repetitionsPerSet(value) {
+        validateInteger(value, ROUTINE_VALIDATION.repetitionsPerSet);
+        this.#repetitionsPerSet = value;
+    }
+
+    get rest() {
+        return this.#rest;
+    }
+
+    set rest(value) {
+        validateInteger(value, ROUTINE_VALIDATION.rest);
+        this.#rest = value;
+    }
+
+    get durationRoutine() {
+        return this.#durationRoutine;
+    }
+
+    rename(newName) {
+
+        this.name = newName;
+    }
+
+    changeCountSeries(newValue) {
+    
+        this.series = newValue;
+
+        this.#durationRoutine = this.calculateDuration();
+    }
+
+    changeRepetitionsPerSet(newValue) {
+    
+        this.repetitionsPerSet = newValue;
+
+        this.#durationRoutine = this.calculateDuration();
+    }
+
+    changeRest(newValue) {
+
+        this.rest = newValue;
+
+        this.#durationRoutine = this.calculateDuration();
+    }
+
+    calculateDuration() {
+        return ((this.#repetitionsPerSet * this.#durationPerSet) * this.#series) + ((this.#series - 1) * this.#rest);
+    }
+
+    static generateId() {
+        return ++Routine.#idCounter;
+    }
+
+
 }
 
-function Routine(name, series, repetitionsPerSet, rest) {
-
-    validateRoutine(name, series, repetitionsPerSet, rest);
-
-    // Propiedades
-    this.id = ++routineIdCounter;
-    this.name = name.trim();
-    this.durationPerSet = 5;
-    this.series = series;
-    this.repetitionsPerSet = repetitionsPerSet;
-    this.rest = rest;
-    this.durationRoutine = this.calculateDuration();
-    this.createdAt = new Date().toISOString();
-
-
-}
-
-// Comportamientos
-Routine.prototype.rename = function rename(newName) {
-    validateName(newName);
-    this.name = newName.trim();
-
-}
-
-Routine.prototype.changeCountSeries = function changeCountSeries(newValue) {
-    validateInteger(newValue, ROUTINE_VALIDATION.series);
-
-    this.series = newValue;
-    this.durationRoutine = this.calculateDuration();
-
-}
-
-Routine.prototype.changeRepetitionsPerSet = function changeRepetitionsPerSet(newValue) {
-    validateInteger(newValue, ROUTINE_VALIDATION.repetitionsPerSet);
-
-    this.repetitionsPerSet = newValue;
-    this.durationRoutine = this.calculateDuration();
-
-}
-
-Routine.prototype.changeRest = function changeRest(newValue) {
-    validateInteger(newValue, ROUTINE_VALIDATION.rest);
-
-    this.rest = newValue;
-    this.durationRoutine = this.calculateDuration();
-
-}
-
-Routine.prototype.calculateDuration = function calculateDuration() {
-
-    return ((this.repetitionsPerSet * this.durationPerSet) * this.series) + ((this.series - 1) * this.rest);
-}
 
 let messageTimeout;
 
@@ -110,7 +154,7 @@ function showMessage(message, type = 'success') {
     }
 
     clearTimeout(messageTimeout);
-    
+
 
     box.textContent = message;
     box.className = `message-box ${type}`;
@@ -155,7 +199,7 @@ function renderRoutines() {
 
 
     if (routines.length === 0) {
-       
+
         const emptyState = document.createElement('p');
         emptyState.className = 'empty-state'
         emptyState.textContent = 'No hay rutinas. Haz clic en "Crear Rutina" para comenzar.';
